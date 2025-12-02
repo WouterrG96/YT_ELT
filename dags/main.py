b24ec55430babba7a4b83118e09dc0d8cd04ecef
WouterrG <wouterdegruijl@quicknet.nl>
2025-12-02 10:37:01 +0100

    Data quality tests using Soda working as DAG

--- dags/main.py
+++ dags/main.py
@@ -11,6 +11,7 @@ from api.video_stats import (
 )
 
 from datawarehouse.dwh import staging_table, core_table
+from dataquality.soda import yt_elt_data_quality
 
 # Define the local timezone
 local_tz = pendulum.timezone("Europe/Malta")
@@ -29,6 +30,10 @@ default_args = {
     "start_date": datetime(2025, 1, 1, tzinfo=local_tz),
     # 'end_date': datetime(2030, 12, 31, tzinfo=local_tz),
 }
+
+# Variables
+staging_schema = "staging"
+core_schema = "core"
 
 # DAG 1: produce_json
 with DAG(
@@ -76,6 +81,34 @@ with DAG(
     # Task 2: Read staging, transform, then upsert into core schema/table (curated model)
     update_core = core_table()
 
+    # Task 3: Trigger the data quality DAG after DB updates complete
+    trigger_data_quality = TriggerDagRunOperator(
+        task_id="trigger_data_quality",
+        trigger_dag_id="data_quality",
+    )
+
     # --- Define dependencies ---
     # Ensure staging update happens before core, and data quality runs last
-    update_staging >> update_core
+    update_staging >> update_core >> trigger_data_quality
+
+
+# DAG 3: data_quality
+with DAG(
+    dag_id="data_quality",
+    default_args=default_args,
+    description="DAG to check the data quality on both layers in the database",
+    catchup=False,
+    # No schedule => this DAG is triggered manually or by another DAG/operator
+    schedule=None,
+) as dag_quality:
+
+    # --- Define tasks ---
+    # Task 1: Run Soda checks against the staging schema
+    soda_validate_staging = yt_elt_data_quality(staging_schema)
+
+    # Task 2: Run Soda checks against the core schema
+    soda_validate_core = yt_elt_data_quality(core_schema)
+
+    # --- Define dependencies ---
+    # Ensure staging checks run before core checks
+    soda_validate_staging >> soda_validate_core
